f13a6d1323061d87944d8d0147a5c4098fe8e9a6
Thibaud Desodt <tibo.desodt@gmail.com>
2018-05-25 21:21:45 +0200

    initial scaffholding

---
+++ src/SerilogAuditee/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SerilogAuditee
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/SerilogAuditee.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SerilogAuditee.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
